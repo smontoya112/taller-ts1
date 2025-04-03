9521d3a2bb4e4f22ebce818eff2c4ab84f7f3d74
Samuel Montoya <s.montoya112@uniandes.edu.co>
2025-04-02 20:19:18 -0500

    h

---
+++ Serie.js
@@ -0,0 +1,12 @@
+"use strict";
+class Serie {
+    constructor(id, title, network, season, description, url, image) {
+        this.id = id;
+        this.title = title;
+        this.network = network;
+        this.season = season;
+        this.description = description;
+        this.url = url;
+        this.image = image;
+    }
+}
